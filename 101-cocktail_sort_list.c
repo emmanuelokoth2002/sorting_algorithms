fe1fc3bc5d46aa1e070b0953dcae609dee154074
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-04-25 19:59:17 -0700

    Task 5

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,85 +1,72 @@
 #include "sort.h"
 
 /**
- * swap_nodes - swap nodes
- * @list: pointer list
- * @node: pointer node
+ * cocktail_sort_list - Sorts a doubly linked list of integers
+ *                      in ascending order using the Cocktail shaker sort algorithm
+ *
+ * @list: Pointer to the first element of the list
+ *
+ * Return: void
  */
-
-void swap_nodes(listint_t **list, listint_t *node)
-{
-	node->next->prev = node->prev;
-
-	if (node->next->prev)
-		node->prev->next = node->next;
-	else
-		*list = node->next;
-
-	node->prev = node->next;
-	node->next = node->next->next;
-	node->prev->next = node;
-
-	if (node->next)
-		node->next->prev = node;
-}
-
-/**
- * get_length - compute  of nodes in list
- * @list: the double list
- * Return: Number of nodes
- */
-
-listint_t *get_length(listint_t *list)
-{
-	listint_t *curr = list;
-
-	while (curr->next != NULL)
-		curr = curr->next;
-
-	return (curr);
-}
-
-/**
- * cocktail_sort_list - sort doubly linked list
- * @list: pointer to list
- */
-
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *curr = NULL, *left_limit = NULL, *right_limit = NULL;
-	int cycle_type = INCREMENT;
+	int swapped;
+	listint_t *curr;
+	listint_t *temp;
 
-	if (!list || !(*list) || !(*list)->next)
+	if (list == NULL || *list == NULL)
 		return;
 
-	curr = *list;
-	left_limit = curr;
-	right_limit = get_length(*list);
-
-	while (left_limit != right_limit)
+	do 
 	{
-		if (curr->n == curr->next->n)
-			break;
-		else if (curr->n > curr->next->n && cycle_type == INCREMENT)
-			swap_nodes(list, curr), print_list(*list);
-		else if (curr->next->n < curr->n && cycle_type == DECREMENT)
-			swap_nodes(list, curr), curr = curr->prev, print_list(*list);
-		else if (cycle_type == INCREMENT)
-			curr = curr->next;
-		else if (cycle_type == DECREMENT)
-			curr = curr->prev;
-
-		if (cycle_type == DECREMENT && curr->next == left_limit)
+		swapped = 0;
+		
+		for (curr = *list; curr->next != NULL; curr = curr->next)
 		{
-			cycle_type = INCREMENT;
-			curr = curr->next;
-		}
+			if (curr->n > curr->next->n)
+			{
+				temp = curr->next;
+				curr->next = temp->next;
+			if (temp->next != NULL)
+				temp->next->prev = curr;
+		temp->prev = curr->prev;
+		if (curr->prev != NULL)
+			curr->prev->next = temp;
+		else
+			*list = temp;
+		curr->prev = temp;
+		temp->next = curr;
 
-		if (cycle_type == INCREMENT && curr->prev == right_limit)
+		swapped = 1;
+		print_list(*list);
+	}
+	}
+
+	if (swapped == 0)
+		break;
+
+	swapped = 0;
+
+	for (curr = curr->prev; curr != NULL; curr = curr->prev)
+	{
+		if (curr->n > curr->next->n)
 		{
-			right_limit = right_limit->prev;
-			cycle_type = DECREMENT;
-			curr = curr->prev;
+		temp = curr->next;
+		curr->next = temp->next;
+		if (temp->next != NULL)
+			temp->next->prev = curr;
+		temp->prev = curr->prev;
+		if (curr->prev != NULL)
+			curr->prev->next = temp;
+		else
+			*list = temp;
+		curr->prev = temp;
+		temp->next = curr;
+
+		swapped = 1;
+		print_list(*list);
 		}
 	}
+	}
+	while (swapped);
 }
